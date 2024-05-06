b88eede9fab4bd24679d9d10b0d1ce72772a7685
Adam L. <144614242+AStellarScene@users.noreply.github.com>
2024-05-06 18:54:56 -0400

    Added sound effects and music.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var jump_player = $JumpPlayer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -16,6 +17,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_player.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
